e19b6e0a1f15d3b3ed5a3257839890be7e636dee
Nick Jackson <nickdjackson@msn.com>
2022-10-11 15:42:22 -0400

    searching

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,20 @@
 import { MongoClient } from 'mongodb'
 
-import {uri } from "./credentials.js"
+import {uri, otherSecret } from "./credentials.js"
 
 const client = new MongoClient(uri)
 
-const db = client.db("test")
+const db = client.db("sample_mflix")
+const moviesCollection = db.collection("movies")
+
+// This is how I will search through MongoDB
+let query = {title: { $regex: /terminator/i}} //Search for terminator anywhere in the title and ignore case
+let movieArray = await moviesCollection.find(query).limit(3).toArray()
+let firstMovie = movieArray[0]
+//console.log(`There are ${movieArray.length} number of movies`)
+
+for(let i = 0; i < movieArray.length; i++) {
+    console.log(movieArray[i].title)
+}
+
+// console.log(firstMovie.title)
