2ac6cd7e704ffafc82267d09864c8c5adb368ac3
Nick Jackson <nickdjackson@msn.com>
2022-10-11 16:54:37 -0400

    stop

--- mongodb.js
+++ mongodb.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient,ObjectId } from 'mongodb'
 
 import {uri, otherSecret } from "./credentials.js"
 
@@ -18,3 +18,20 @@ for(let i = 0; i < movieArray.length; i++) {
 }
 
 // console.log(firstMovie.title)
+
+
+// add a new movie
+const newMovie = {
+    title: "The Boca Code Story",
+    rating: "R",
+    genre: "Comedy",
+    releaseDate: "2022/12/16"
+}
+
+// const results = await moviesCollection.insertOne(newMovie)
+// console.log("Results of insert", results)
+
+const updateQuery = {_id: new ObjectId("6345ca5fdd08efdf1ce5bff8")}
+const update = { $inc: {oscars: 1}} 
+const results = await moviesCollection.findOneAndUpdate(updateQuery,update);
+console.log(results)
